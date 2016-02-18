1d4aedcc49306d82c90e2684be54c83643f8c91c
Steven Bedrick <steve@bedrick.org>
2016-02-18 11:34:48 -0800

    Version bump for openfst and opengrm-thrax

--- opengrm-thrax.rb
+++ opengrm-thrax.rb
@@ -1,7 +1,7 @@
 class OpengrmThrax < Formula
   homepage "http://www.openfst.org/twiki/bin/view/GRM/Thrax"
-  url "http://www.openfst.org/twiki/pub/GRM/ThraxDownload/thrax-1.2.0.tar.gz"
-  sha256 "23112837b64634685e34681758b42b55d09b97b999c2f8a43c0002b870f98fc9"
+  url "http://www.openfst.org/twiki/pub/GRM/ThraxDownload/thrax-1.2.1.tar.gz"
+  sha256 "2aaf9bcabe7ad2193ba77d048d7e70a434888c0397ed337f1a01bf0f26549656"
 
   bottle do
     cellar :any
