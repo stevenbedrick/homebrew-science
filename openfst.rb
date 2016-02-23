9b8abb3e708945557c3879a3baad69ff76ebdbf3
Steven Bedrick <steve@bedrick.org>
2016-02-22 21:54:57 -0800

    Version bump for OpenFST 1.5.1, OpenGRM-Thrax 1.2.1, and OpenGRM-NGram 1.2.2

--- openfst.rb
+++ openfst.rb
@@ -1,7 +1,7 @@
 class Openfst < Formula
   homepage "http://www.openfst.org/"
-  url "http://openfst.org/twiki/pub/FST/FstDownload/openfst-1.5.0.tar.gz"
-  sha256 "01c2b810295a942fede5b711bd04bdc9677855c846fedcc999c792604e02177b"
+  url "http://openfst.org/twiki/pub/FST/FstDownload/openfst-1.5.1.tar.gz"
+  sha256 "d40724bc5641091063db632392e6561e9077d5e0d88ac106d5b30d8f1bcd9925"
 
   bottle do
     cellar :any
