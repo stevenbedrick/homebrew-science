e3bc9294758e8bd1bb0d9714c389d7d2e583f1ee
Steven Bedrick <steve@bedrick.org>
2016-02-24 12:02:50 -0800

    Removing revision statement as per CI bot's instructions.

--- opengrm-thrax.rb
+++ opengrm-thrax.rb
@@ -2,7 +2,6 @@ class OpengrmThrax < Formula
   homepage "http://www.openfst.org/twiki/bin/view/GRM/Thrax"
   url "http://www.openfst.org/twiki/pub/GRM/ThraxDownload/thrax-1.2.1.tar.gz"
   sha256 "3710feefe7bdb462b032b169946b8338a4c1220181a5dcc3ef798d5d09adccdc"
-  revision 1
 
   bottle do
     cellar :any
